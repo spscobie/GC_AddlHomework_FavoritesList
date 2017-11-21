8f64f1697479cb975a0525c9badb5c457ff5e493
spscobie <stephenscobie@gmail.com>
2017-11-20 20:14:52 -0500

    Initial commit.

---
+++ Day20_Lecture_FavoritesList/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day20_Lecture_FavoritesList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //this will take care of the action:
+        public ActionResult AddToFavsList (string Name)
+        {
+            //set up a session
+            if (Session["FavList"] == null)
+            {
+                Session.Add("FavList", new List<string>());
+            }
+
+            // fetch the list from the session
+            List<string> FavList = (List<string>)Session["FavList"];
+
+            //Check for duplication. If none then add song to the list.
+            if (!FavList.Contains(Name))
+            {
+                FavList.Add(Name); //added a song to the favorites list
+            }
+
+            FavList.Sort();
+            Session["FavList"] = FavList; //added list to the session
+
+            ViewBag.FavList = FavList;
+
+            return View("About");
+        }
+    }
+}
---
+++ Day20_Lecture_FavoritesList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Day20_Lecture_FavoritesList.MvcApplication" Language="C#" %>
